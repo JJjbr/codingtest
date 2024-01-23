c1a768a6042e8a66facfa655e8291587083f27a7
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-01-23 14:41:39 +0900

    Update 1935.cpp

--- baekjoon/1935.cpp
+++ baekjoon/1935.cpp
@@ -7,9 +7,8 @@ int main()
 {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int N;
-	cin >> N;
 	string exp;
-	cin >> exp;
+	cin >> N >> exp;
 	int num[26] = { 0, };
 	for (int i = 0; i < N; i++)
 	{
