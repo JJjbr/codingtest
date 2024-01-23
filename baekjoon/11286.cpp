aab2173c838b0d933589e070351d090db5219cf8
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-01-24 00:26:50 +0900

    Update 11286.cpp

--- baekjoon/11286.cpp
+++ baekjoon/11286.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
 
+using namespace std;
+
+struct cmp {
+	bool operator()(int a, int b) {
+		int a_ = abs(a), b_ = abs(b);
+		if (a_ == b_) return a > b;
+		return a_ > b_;
+	}
+};
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	priority_queue<int, vector<int>, cmp> q;
+	int x;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x;
+		if (x)
+		{
+			q.push(x);
+		}
+		else
+		{
+			if (!q.empty()) {
+				cout << q.top() << "\n";
+				q.pop();
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+	}
+	return 0;
+}
