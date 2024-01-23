0ce11b3b3fc4aff75e84a36eca374ba9130a8637
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-01-23 14:31:51 +0900

    Create 1935.cpp

---
+++ 1935.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N;
+	cin >> N;
+	string exp;
+	cin >> exp;
+	int num[26] = { 0, };
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+
+	string op = "+-/*";
+	vector<double> box;
+	for (int i = 0; i < exp.length(); i++)
+	{
+		if (op.find(exp[i]) != string::npos)
+		{
+			double op2 = box.back();
+			box.pop_back();
+			double op1 = box.back();
+			box.pop_back();
+			if (exp[i] == '+')
+				box.push_back(op1 + op2);
+			else if (exp[i] == '-')
+				box.push_back(op1 - op2);
+			else if (exp[i] == '*')
+				box.push_back(op1 * op2);
+			else
+				box.push_back(op1 / op2);
+		}
+		else
+		{
+			box.push_back(num[exp[i] - 'A']);
+		}
+	}
+	printf("%.2f", box[0]);
+	return 0;
+}
