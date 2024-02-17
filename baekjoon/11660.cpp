cced595a5baf4df78f6073fa4e5b59526a595e54
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-02-17 20:17:19 +0900

    Update 11660.cpp

--- baekjoon/11660.cpp
+++ baekjoon/11660.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int map[1026][1026];
+int sum[1026][1026];
 
 int main()
 {
@@ -11,30 +12,13 @@ int main()
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
 			cin >> map[i][j];
-			if (j > 1) {
-				map[i][j] += map[i][j - 1];
-			}
+			sum[i][j] = map[i][j] + sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1];
 		}
 	}
-	for (int i = 2; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			map[i][j] += map[i - 1][j];
-		}
-	}
-	int x1, y1, x2, y2, sum;
-	for (int m = 0; m < M; m++) {
+	int x1, y1, x2, y2;
+	for (int i = 0; i < M; i++) {
 		cin >> x1 >> y1 >> x2 >> y2;
-		sum = map[x2][y2];
-		if (y1 > 1) {
-			sum -= map[x2][y1 - 1];
-		}
-		if (x1 > 1) {
-			sum -= map[x1 - 1][y2];
-		}
-		if (x1 > 1 && y1 > 1) {
-			sum += map[x1 - 1][y1 - 1];
-		}
-		cout << sum << "\n";
+		cout << sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][y1 - 1] << "\n";
 	}
 	return 0;
 }
